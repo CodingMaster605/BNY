a23b35704a5e1a85c39fa0532dadae3aa13f4a8c
iMemez <58692795+CodingMaster605@users.noreply.github.com>
2020-12-12 10:20:13 +0400

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 // Put your token here
-export let token: string = "NzY4MTg5NDcwMzIyNzIwODA4.X482MQ.HXzb4WGKYlWFg_3c3sYa-uNl1sM";
+export let token: string = "token";
 // Replace ! with the prefix you would like to use.
 export let prefix: string = "(";
 // Add your user id to the owners variable. It is an array, so you can have multiple owners.
