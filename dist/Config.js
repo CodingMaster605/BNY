3dd56ac440442488c338375d7170caa8ed25a98a
iMemez <58692795+CodingMaster605@users.noreply.github.com>
2020-12-12 10:21:22 +0400

    Update Config.js

--- dist/Config.js
+++ dist/Config.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.owners = exports.prefix = exports.token = void 0;
-// Put your token here
-exports.token = "NzY4MTg5NDcwMzIyNzIwODA4.X482MQ.HXzb4WGKYlWFg_3c3sYa-uNl1sM";
-// Replace ! with the prefix you would like to use.
+exports.token = "token";
 exports.prefix = "(";
-// Add your user id to the owners variable. It is an array, so you can have multiple owners.
 exports.owners = [];
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL0NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxzQkFBc0I7QUFDWCxRQUFBLEtBQUssR0FBVyw2REFBNkQsQ0FBQztBQUN6RixtREFBbUQ7QUFDeEMsUUFBQSxNQUFNLEdBQVcsR0FBRyxDQUFDO0FBQ2hDLDRGQUE0RjtBQUNqRixRQUFBLE1BQU0sR0FBYSxFQUFFLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBQdXQgeW91ciB0b2tlbiBoZXJlXHJcbmV4cG9ydCBsZXQgdG9rZW46IHN0cmluZyA9IFwiTnpZNE1UZzVORGN3TXpJeU56SXdPREE0Llg0ODJNUS5IWHpiNFdHS1lsV0ZnXzNjM3NZYS11Tmwxc01cIjtcclxuLy8gUmVwbGFjZSAhIHdpdGggdGhlIHByZWZpeCB5b3Ugd291bGQgbGlrZSB0byB1c2UuXHJcbmV4cG9ydCBsZXQgcHJlZml4OiBzdHJpbmcgPSBcIihcIjtcclxuLy8gQWRkIHlvdXIgdXNlciBpZCB0byB0aGUgb3duZXJzIHZhcmlhYmxlLiBJdCBpcyBhbiBhcnJheSwgc28geW91IGNhbiBoYXZlIG11bHRpcGxlIG93bmVycy5cclxuZXhwb3J0IGxldCBvd25lcnM6IHN0cmluZ1tdID0gW107Il19
